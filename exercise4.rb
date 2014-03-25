19ab39e464994556266cba4433bd196823092354
Senthil <senthil@erptek.com>
2014-03-25 16:14:54 -0400

    Exercise 3 completed

--- exercise4.rb
+++ exercise4.rb
@@ -1,6 +1,13 @@
+no = 0..99
+no.each do|x| x +=1
+  if (x%3).zero? && (x%5).zero?
+	puts "BitMaker"
+  elsif (x%3).zero?
+    puts "Bit"
+  elsif (x%5).zero?
+    puts "Maker"
+  else
+	puts "#{x}"
+  end
+end
 
-puts "How old are you?"
-age = gets.chomp.to_i
-time=Time.new
-puts "#{time.year-age}"
-#puts " You are born in Year #{year-age}"
