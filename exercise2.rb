517ce1d08ca603a546aa8965d4f7bf041f9e1d90
Senthil <senthil@erptek.com>
2014-03-25 13:34:02 -0400

    Second Exercise

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,11 @@
+#Exercise No. 1
 puts "Enter the total cost of meals"
 meals= gets.chomp.to_i
 puts "The tip is $#{meals*0.1}"
+
+#Exercise No. 2
+x = "Hello"
+y = 3
+puts "#{x} + #{y.to_s}"
+puts "#{x + y.to_s}"
+
